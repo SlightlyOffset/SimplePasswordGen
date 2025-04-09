7bf48a62130a402e717db5bc33c56cb219be94aa
TurboVirgin <69239449+Mamoungxd@users.noreply.github.com>
2025-04-09 15:13:31 +0700

    Create SimplePasswordGen.py
    make a template for program

---
+++ SimplePasswordGen.py
@@ -0,0 +1,12 @@
+'''
+Simple Password Generator:
+    Create a program that generates random passwords of a specified length, with options for including uppercase letters, lowercase letters, numbers, and symbols.
+'''
+
+def main():
+    pass
+    
+    
+    
+if __name__ == "__main__":
+    main()
